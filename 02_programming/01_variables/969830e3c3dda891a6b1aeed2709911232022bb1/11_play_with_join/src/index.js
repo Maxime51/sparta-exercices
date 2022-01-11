83bc44a37a5ecaf99aeefe73bc5aaff7e4ba9841
mansuy maxime <mmansuy@norauto.fr>
2022-01-11 15:59:24 +0100

    Add file 11

---
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/11_play_with_join/src/index.js
@@ -0,0 +1,16 @@
+// create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
+const litteralDigits = [];
+litteralDigits.push("zero");
+litteralDigits.push("one");
+litteralDigits.push("two");
+litteralDigits.push("three");
+litteralDigits.push("four");
+litteralDigits.push("five");
+litteralDigits.push("six");
+litteralDigits.push("seven");
+litteralDigits.push("eight");
+litteralDigits.push("nine");
+
+
+// use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
+const allDigits = litteralDigits.join(" - ");
