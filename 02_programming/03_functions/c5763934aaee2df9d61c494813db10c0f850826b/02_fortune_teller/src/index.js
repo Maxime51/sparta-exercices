04fb3c84468f926d55f4b0258b2699d5cf8dd400
mansuy maxime <mmansuy@norauto.fr>
2022-01-13 11:20:09 +0100

    Add file 02

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -0,0 +1,7 @@
+function tellFortune(param1,param2,param3,param4) {
+  // Code the function here.
+  console.log(`You will be ${param1} in ${param2}, and married to ${param3} in ${param4} years.`);
+}
+tellFortune("developper","Reims","Rapha","3");
+// Do not remove last lines, it is for tests
+module.exports = tellFortune;
