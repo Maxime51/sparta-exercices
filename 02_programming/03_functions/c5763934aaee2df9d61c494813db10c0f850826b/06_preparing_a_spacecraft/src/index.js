50e1f8c7352b60bdc254f5b9a56262cdd2adce66
mansuy maxime <mmansuy@norauto.fr>
2022-01-13 16:59:37 +0100

    Add file 06

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -0,0 +1,26 @@
+function checkFuelLevel(number) {
+  // Code here
+  if(number >= 20){
+    return "Fuel level: green";
+  }else if (number > 10 && number < 20){
+    return "Fuel level: yellow";
+  }else{
+    return "Fuel level: red";
+  }
+}
+
+function checkCargoHold(cargo){
+  // Code here
+  const fullSpaceCargo = 4;
+  console.log(cargo.length);
+  if(cargo.length === fullSpaceCargo){
+    return "Full";
+  }else if(cargo.length < fullSpaceCargo){
+    return `Spaces available: ${fullSpaceCargo-cargo.length}`;
+  }else{
+    return `Over capacity by ${cargo.length - fullSpaceCargo} items.`;
+  }
+}
+
+// Do not remove last lines, it is for tests
+module.exports = { checkFuelLevel, checkCargoHold };
