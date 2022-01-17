f1a50d7ed4010e95daa3f733376f6429f76ee003
mansuy maxime <mmansuy@norauto.fr>
2022-01-17 10:56:01 +0100

    Add file 01

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -0,0 +1,11 @@
+
+function greeter(people) {
+  // Code the function here.
+  people.forEach(element => {
+    console.log(`Hello ${element}!`);
+  });
+
+}
+
+// Do not remove the following lines, it is for tests
+module.exports = greeter;
