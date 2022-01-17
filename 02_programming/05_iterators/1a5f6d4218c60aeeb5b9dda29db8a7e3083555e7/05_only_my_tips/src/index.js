7c803e92a971290a7046120ea81afeafaf86ad39
mansuy maxime <mmansuy@norauto.fr>
2022-01-17 13:58:00 +0100

    05_only_my_tips

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
@@ -0,0 +1,15 @@
+function onlyMyTips(invoices) {
+  // Code here
+  const listYourself = invoices.filter(list => list.waiter === "Yourself");
+  const tipResult = listYourself.map(tip => tip.tip);
+  return tipResult;
+}
+
+const todaysInvoices = [
+  { clients: 3, waiter: "Yourself", price: 5100, tip: 200 },
+  { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
+  { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
+];
+
+// Do not remove the following line, it is for tests
+module.exports = onlyMyTips;
