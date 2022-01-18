376332a161073e4635c6d35162a9a0fe154f75fe
mansuy maxime <mmansuy@norauto.fr>
2022-01-18 17:49:13 +0100

    Add file 09 not finish

---
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/09_colorful_numbers_bonus/src/colorful.js
@@ -0,0 +1,22 @@
+function isColorful(number) {
+  // Code here
+
+  for (let i = 0; i <number.length; i++){
+
+    for (let y = 0; y < number.length-1; y++){
+
+      for (let z = 0; z < number.length-2; z++){
+
+        for(let p=0; p < number.length-3; p++){
+
+
+
+
+        }
+      }
+    }
+  }
+}
+isColorful("3245");
+// Do not remove last lines, it is for tests
+module.exports = isColorful;
