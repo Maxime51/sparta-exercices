0842cd7e5383576f171f28ce88f16946b82860be
mansuy maxime <mmansuy@norauto.fr>
2022-01-19 09:57:03 +0100

    Maj 09

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/09_colorful_numbers_bonus/src/colorful.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/09_colorful_numbers_bonus/src/colorful.js
@@ -1,18 +1,17 @@
 function isColorful(number) {
   // Code here
 
+  let tab1=[];
+  let tab2=[];
+  let tab3=[];
+  let tab4=[];
   for (let i = 0; i <number.length; i++){
+    tab1.push(number[i]);
+    for (let y = i+1; y < number.length-1; y++){
+      tab2.push(number[y]*tab1[i]);
+      for (let z = y+1; z < number.length-2; z++){
+        tab3.push(number[z]*tab2[y-1]);
 
-    for (let y = 0; y < number.length-1; y++){
-
-      for (let z = 0; z < number.length-2; z++){
-
-        for(let p=0; p < number.length-3; p++){
-
-
-
-
-        }
       }
     }
   }
