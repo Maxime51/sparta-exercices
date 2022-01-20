b92bf09e897816652e89cc1e27c7da7fa85432cc
mansuy maxime <mmansuy@norauto.fr>
2022-01-20 10:03:14 +0100

    Add file 01

---
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/01_fundamental_flowchart/src/shouldIMakeATicket.js
@@ -0,0 +1,21 @@
+function shouldIMakeATicket(isStuck, timeBeingStuck, hourOfTheDay){
+
+  if(isStuck === true){
+    if(timeBeingStuck >= 20){
+      if((hourOfTheDay >= 9 && hourOfTheDay < 13) ||(hourOfTheDay >= 14 && hourOfTheDay < 18) ){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+  // Code your function here
+
+}
+
+// Don't touch this for tests to execute properly
+module.exports = shouldIMakeATicket;
