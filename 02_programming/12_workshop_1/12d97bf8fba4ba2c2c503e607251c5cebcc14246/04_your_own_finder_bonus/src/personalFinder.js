75585b54c26bb91aaff83180eaec7d6af44a6d49
mansuy maxime <mmansuy@norauto.fr>
2022-01-28 12:00:31 +0100

    Add file 04 finish

--- 02_programming/12_workshop_1/12d97bf8fba4ba2c2c503e607251c5cebcc14246/04_your_own_finder_bonus/src/personalFinder.js
+++ 02_programming/12_workshop_1/12d97bf8fba4ba2c2c503e607251c5cebcc14246/04_your_own_finder_bonus/src/personalFinder.js
@@ -7,20 +7,28 @@ const reader = readline.createInterface({
 });
 
 // Code here
-function pwd(){
-  const data = process.cwd().split("/");
+
+function pwd(data = process.cwd().split("/")){
   data[0]= ".";
   data.forEach((element,index) => console.log(`${index} - ${element}`));
   reader.question("Choose a number > \n",keyboard => finder(keyboard,data));
 }
+
 function finder(keyboard,data){
   const lengthOflines = data.length;
   let chaine="";
+  console.log("test =>>>>>>>>>>>>>>",lengthOflines,keyboard);
   for(let i = 1; i < (lengthOflines- parseInt(keyboard)) ; i++){
     chaine = "../"+chaine;
   }
+  if(parseInt(keyboard) === lengthOflines-1){
+    chaine = ".";
+    console.log(fs.readdirSync(chaine));
+  }
+  else{
+    console.log(fs.readdirSync(chaine));
+  }
 
-  console.log(fs.readdirSync(chaine));
-  //console.log(keyboard);
+  pwd(fs.readdirSync(chaine));
 }
 module.exports = {pwd};
