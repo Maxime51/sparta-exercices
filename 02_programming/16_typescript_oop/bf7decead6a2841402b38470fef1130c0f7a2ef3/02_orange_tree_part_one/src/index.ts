bbcbe2dfc2ae9651c85493eae1d7d9efb27f9b9a
mansuy maxime <mmansuy@norauto.fr>
2022-02-01 10:17:58 +0100

    Add file 02 ok

---
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -0,0 +1,30 @@
+// Code the class here.
+class Tree {
+  age: number;
+  height: number;
+  alive: boolean = true;
+
+  constructor(age: number) {
+    this.age = age;
+    if (age === 0) {
+      this.height = 0;
+    } else if (age >= 1 && age <= 9) {
+      this.height = 25 * age;
+    } else if (age >= 10 && age <= 20) {
+      this.height = 25 * 9 + 10 * (age - 9);
+    } else if (age > 20) {
+      this.height = 25 * 9 + 10 * (age - 9);
+    }
+  }
+
+  seed(): void {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+  }
+}
+// Leave the line below for tests to work properly.
+
+const tree = new Tree(20);
+console.log(tree.height);
+export { Tree };
