bf7376e23f350e2b96ae3bc2638956a627f76558
mansuy maxime <mmansuy@norauto.fr>
2022-02-02 12:00:12 +0100

    Add file 05 ok

---
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
@@ -0,0 +1,16 @@
+function filterFor<ArrayElementType>(
+  array: ArrayElementType[],
+  predicate: (element: ArrayElementType) => boolean,
+): ArrayElementType[] {
+  const resultArray = [];
+
+  for (const element of array) {
+    if (predicate(element)) {
+      resultArray.push(element);
+    }
+  }
+
+  return resultArray;
+}
+
+export { filterFor };
