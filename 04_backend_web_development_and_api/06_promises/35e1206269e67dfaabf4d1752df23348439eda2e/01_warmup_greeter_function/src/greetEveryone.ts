d935cbffbe43b9824488dc0ed67cedd7916094bc
mansuy maxime <mmansuy@norauto.fr>
2022-02-21 09:57:58 +0100

    add file 01

---
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
@@ -0,0 +1,8 @@
+type ParamRest = string[];
+
+export function greetEveryone(...ParamRest: string[]): void {
+  // Code your function here
+  ParamRest.forEach((element) => {
+    console.log(`"Welcome to ${element}"`);
+  });
+}
